abfaa29c689723abc5e04759b27525a2b3cd1706
teamhaircut <teamhaircutdiginet@gmail.com>
2022-01-05 20:08:12 -0700

    hour6

--- public/DOM/dom.js
+++ public/DOM/dom.js
@@ -2,7 +2,29 @@ function markSquare(square) {
     square.innerHTML="X";
 };
 
-function updateRoomGrid(roomGrid) {
+function updateRoomGrid(roomGrid, last) {
+    console.log(last);
+    console.log(roomGrid);
+    if(last=="grid1") {
+        grid1.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid2"){
+        grid2.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid3"){
+        grid3.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid4"){
+        grid4.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid5"){
+        grid5.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid4"){
+        grid6.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid7"){
+        grid7.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid8"){
+        grid8.classList.add('animate__animated', 'animate__flip');
+    } else if(last=="grid9"){
+        grid9.classList.add('animate__animated', 'animate__flip');
+    }
+    
     grid1.innerHTML=roomGrid[0];
     grid2.innerHTML=roomGrid[1];
     grid3.innerHTML=roomGrid[2];
